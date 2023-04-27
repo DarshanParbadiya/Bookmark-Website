5326f4ab0f242a592a204384a844c7794ebee18b
Darshan <darshanparbadiya@gmail.com>
2023-04-27 11:10:33 +0530

    after clicking items in mobile menu hide the overlay

--- js/script.js
+++ js/script.js
@@ -8,52 +8,55 @@ const panels = document.querySelectorAll(".panel");
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
 function onTabClick(e) {
-     
-//   Deactivate all tabs
+  //   Deactivate all tabs
   tabs.forEach((tab) => {
     // console.log(tab.children[0]); take it's first children
-    tab.children[0].classList.remove(
-        'border-b-4',
-        'border-red-500',
-    )
+    tab.children[0].classList.remove("border-b-4", "border-red-500");
   });
-// Hide all panels
-panels.forEach((panel)=>panel.classList.add('hidden'))
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add("hidden"));
 
-
-// Activate a new tab and panel based on the target
-e.target.classList.add('border-red-500','border-b-4')
-const classString = e.target.getAttribute('data-target')
-document.getElementById('panels')
-.getElementsByClassName(classString)[0]
-.classList.remove('hidden')
+  // Activate a new tab and panel based on the target
+  e.target.classList.add("border-red-500", "border-b-4");
+  const classString = e.target.getAttribute("data-target");
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
 }
-
-
 
 // ------------- hamburger menu -----------------
 
-const menuBtn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
- 
-menuBtn.addEventListener('click',toggleBtn)
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", toggleBtn);
 
 //----------------- Swapping Logo -------------------
 
-const logo = document.getElementById('logo')
+const logo = document.getElementById("logo");
 
-function toggleBtn(){
-  menuBtn.classList.toggle('open')
-  menu.classList.toggle('hidden')
-  menu.classList.toggle('white-logo')
-  if(menu.classList.contains('white-logo')){
-    logo.setAttribute('src','./images/logo-bookmark-footer.svg')
-
+function toggleBtn() {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("white-logo");
+  if (menu.classList.contains("white-logo")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
   }
-  else{
-    logo.setAttribute('src',"./images/logo-bookmark.svg")
-  }
-  
-  
-}  
+}
 
+// -------------------- after clicking mobile menu items disable it-------------
+// const menuid = document.getElementById("menu");
+console.log(menu.children[0]);
+
+for(i=0;i<=3;i++){
+  menu.children[i].addEventListener('click',disableOverlay)
+}
+function disableOverlay(e){
+    
+    // console.log(e.target)
+    menuBtn.classList.toggle("open");
+    menu.classList.toggle("hidden");
+}
